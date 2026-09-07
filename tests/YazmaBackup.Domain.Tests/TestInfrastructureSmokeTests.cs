04e5dac82d40f3f4c07ff1d3f9b82131f72c3328
Gursoy_Group <bilgiislem@gursoyoto.com.tr>
2026-09-07 22:26:42 +0300

    R28.0 add first automated test gate

---
+++ tests/YazmaBackup.Domain.Tests/TestInfrastructureSmokeTests.cs
@@ -0,0 +1,10 @@
+namespace YazmaBackup.Domain.Tests;
+
+public sealed class TestInfrastructureSmokeTests
+{
+    [Fact]
+    public void XUnit_runner_is_operational()
+    {
+        Assert.True(true);
+    }
+}
