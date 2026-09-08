6e4254c800fa28ae8264f82cb6aeb9aa768a19d4
Gursoy_Group <bilgiislem@gursoyoto.com.tr>
2026-09-08 21:51:23 +0300

    R28.9.2 satisfy test analyzer naming policy

--- tests/YazmaBackup.ControlPlane.Tests/StateStoreTestScope.cs
+++ tests/YazmaBackup.ControlPlane.Tests/StateStoreTestScope.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Hosting;
 namespace YazmaBackup.ControlPlane.Tests;
 
 [CollectionDefinition("ControlPlane environment", DisableParallelization = true)]
-public sealed class ControlPlaneEnvironmentCollection;
+public sealed class ControlPlaneEnvironmentFixture;
 
 internal sealed class StateStoreTestScope : IDisposable
 {
